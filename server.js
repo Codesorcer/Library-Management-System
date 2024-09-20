d446d734f2e91755a5630a3485d5e674caefd5d1
Codesorcer <Rudraprataps7789@gmail.com>
2024-09-20 18:04:43 +0530

    upate the server to express server

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-// Create an HTTP server
-const server = http.createServer((req, res) => {
-    // Set the response headers
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+app.get('/', (req, res) => {
+  res.send('<h1>Hello, Express.js Server!</h1>')
+})
 
-    // Write the response content
-    res.write('<h1>Hello, Node.js HTTP Server!</h1>');
-    res.end();
-});
-
-// Specify the port to listen on
-const port = 3000;
-
-// Start the server
-server.listen(port, () => {
-    console.log(`Node.js HTTP server is running on port ${port}`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
